47cf8ec6c83b0ff11d2d3f09a9d67b6e5c365469
SomnathkumarA <114069124+SomnathkumarA@users.noreply.github.com>
2023-02-24 11:20:21 +0530

    Add files via upload

---
+++ day-2a/src/main/java/com/example/demo/properties.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class properties {
+	@Value("This is my name")
+	private String s;
+	@GetMapping("/display")
+public String show() {
+	return s;
+}
+}
